9e6b63a3f87cca8d2ec816fda8e5a42aaeb84146
Abhishek Singh <abhishek.s.builds@gmail.com>
2026-06-27 08:23:34 +0530

    Add Two Sum solution using unordered_map
    Implemented the Two Sum problem using an unordered_map to achieve O(n) time complexity.

---
+++ 01-two-pointers/01-two-sum.cpp
@@ -0,0 +1,23 @@
+// LeetCode 1 - Two Sum
+// Approach: Using unordered_map to store elements and find complement in O(1)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.count(complement)) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+};
