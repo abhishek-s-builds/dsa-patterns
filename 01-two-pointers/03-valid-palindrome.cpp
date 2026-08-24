a79dbe92333d34d58edffddd3662bc431fb46bf6
Abhishek Singh <abhishek.s.builds@gmail.com>
2026-08-24 23:01:01 +0530

    Add valid palindrome solution using two pointers
    Implement a solution for checking if a string is a palindrome using the two-pointer technique.

---
+++ 01-two-pointers/03-valid-palindrome.cpp
@@ -0,0 +1,34 @@
+// LeetCode Q125 - Valid Palindrome
+// Approach: Two Pointers
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    bool isPalindrome(string s) 
+    {
+
+        int i=0;
+        int j=s.length()-1;
+
+        while(i<j)
+        {
+            while(i<j && !isalnum(s[i]))
+            {
+                i++;
+            }
+            while(i<j && !isalnum(s[j]))
+            {
+                j--;
+            }
+            if(tolower(s[i])!=tolower(s[j]))
+            {
+                return false;
+            }
+
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+};
